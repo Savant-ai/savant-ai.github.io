856284911ca704de829f06af994b3fac0cf6d2ba
SwapDub <swapnildub@gmail.com>
2021-06-07 08:53:29 -0400

    Overlay Working, Requires more formatting

--- js/menu.js
+++ js/menu.js
@@ -18,29 +18,40 @@ menu_item.forEach((item) => {
 	});
 });
 
-// Collapse function javascript
-const triggers = Array.from(document.querySelectorAll('[data-toggle="collapse"]'));
-
-window.addEventListener('click', (ev) => {
-  const elm = ev.target;
-  if (triggers.includes(elm)) {
-    const selector = elm.getAttribute('data-target');
-    collapse(selector, 'toggle');
-  }
-}, false);
-
-
-const fnmap = {
-  'toggle': 'toggle',
-  'show': 'add',
-  'hide': 'remove'
-};
-const collapse = (selector, cmd) => {
-  const targets = Array.from(document.querySelectorAll(selector));
-  targets.forEach(target => {
-    target.classList[fnmap[cmd]]('show');
-  });
+// Overlay Function JavaScript
+function on(num) {
+  document.getElementById("overlay"+ num).style.display = "block";
 }
+
+function off(num) {
+  document.getElementById("overlay"+ num).style.display = "none";
+}
+
+
+// Collapse function javascript
+// Source: https://medium.com/dailyjs/mimicking-bootstraps-collapse-with-vanilla-javascript-b3bb389040e7
+// const triggers = Array.from(document.querySelectorAll('[data-toggle="collapse"]'));
+
+// window.addEventListener('click', (ev) => {
+//   const elm = ev.target;
+//   if (triggers.includes(elm)) {
+//     const selector = elm.getAttribute('data-target');
+//     collapse(selector, 'toggle');
+//   }
+// }, false);
+
+
+// const fnmap = {
+//   'toggle': 'toggle',
+//   'show': 'add',
+//   'hide': 'remove'
+// };
+// const collapse = (selector, cmd) => {
+//   const targets = Array.from(document.querySelectorAll(selector));
+//   targets.forEach(target => {
+//     target.classList[fnmap[cmd]]('show');
+//   });
+// }
 
 
 
